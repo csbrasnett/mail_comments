507fde21106accc03a0341f52c2767fca43d52ef
csbrasnett <christopher.brasnett@bristol.ac.uk>
2019-05-03 16:13:13 +0100

    added markovify features to the analysis

--- analysis.py
+++ analysis.py
@@ -7,19 +7,33 @@ author: Chris Brasnett, University of Bristol, christopher.brasnett@bristol.ac.u
 
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+import markovify
 
-with open('comments2.txt', 'r') as f:
+
+with open('comments2316069.txt', 'r') as f:
     t = f.read()
     
 t1 = t.split('\n')
 
-phrase = ['england']
+phrase = [''] 
 
           
 for p in phrase:
     matches = [s for s in t1 if p in s]
-    
+    print(len(matches))
     text = ''.join(matches)
+    
+    text_model = markovify.Text(text, state_size = 3)
+
+
+    for i in range(20):
+        t = text_model.make_sentence(tries =100)
+        try:
+            if len(t)>5:
+                print(t, '\n')
+        except TypeError:
+            pass
+
     try:
         wordcloud = WordCloud(max_font_size = 100, width = 1000, height = 500).generate_from_text(text)
         
